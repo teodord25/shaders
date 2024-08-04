39ce92e040422441afad119e7d55c825c78ebae0
tedord25 <djuric.teodor25@gmail.com>
2024-08-04 21:12:05 +0200

    set black background and change rect to ellipse

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup() {
 
 function draw() {
 	clear();
+	background(0);
 	// width and height here are automatically set by p5.js to the size of the canvas ?
-	rect(0, 0, width, height);
+	ellipse(0, 0, width, height);
 }
