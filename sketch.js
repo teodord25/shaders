6cb6a9c82c89b02523b03edc2843ff874011eb27
tedord25 <djuric.teodor25@gmail.com>
2024-08-04 20:35:39 +0200

    rm text and load shader

--- sketch.js
+++ sketch.js
@@ -2,22 +2,20 @@ let shaderProgram;
 let font;
 
 function preload() {
-    shaderProgram = loadShader('vertex.vert', 'fragment.frag');
-    font = loadFont('/Ldfcomicsans-jj7l.ttf');
+	shaderProgram = loadShader("vertex.vert", "fragment.frag");
+	font = loadFont("/Ldfcomicsans-jj7l.ttf");
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
+	createCanvas(windowWidth, windowHeight, WEBGL);
 
-    fill(0);
-    textFont(font);
-    text(webglVersion, 0, 0);
+        shader(shaderProgram);
 
-    noStroke();
+	noStroke();
 }
 
 function draw() {
-//    clear();
-// width and height here are automatically set by p5.js to the size of the canvas ?
-    // rect(0, 0, width, height);
+	clear();
+	// width and height here are automatically set by p5.js to the size of the canvas ?
+	rect(0, 0, width, height);
 }
