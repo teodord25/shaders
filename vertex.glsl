1b3e6bed8d0c940d5037032cdbc84521c491696b
tedord25 <djuric.teodor25@gmail.com>
2024-08-04 21:32:43 +0200

    wiggly circle

--- vertex.glsl
+++ vertex.glsl
@@ -21,6 +21,8 @@ void main() {
     // make circle have padding
     pos.xy *= 0.5;
 
+    pos.y += sin(pos.x * 20.)/20.;
+
     // NOTE: this setup currently stretches what is drawn
     // to fill the screen, to have the geometry use the 
     // height and width that it is given, you have to 
