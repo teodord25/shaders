0ee81cbab9757322988c4db2c97eaa989994b54a
tedord25 <djuric.teodor25@gmail.com>
2024-08-16 10:41:23 +0200

    move color stuff to frag shader; rm milis and padding on circle

--- fragment.glsl
+++ fragment.glsl
@@ -1,13 +1,10 @@
 #version 300 es
 precision mediump float;
 
-// inputs from vertex shader
-in vec4 vClr;
-
 // outputs to the framebuffer
 out vec4 outClr;
 
 void main() {
     // set the fragment color to the color passed from the vertex shader
-    outClr = vClr;
+    outClr = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -4,14 +4,7 @@
 in vec3 aPosition;
 in vec2 aTexCoord;
 
-uniform float millis;
-
-// outputs to fragment shader
-out vec4 vClr;
-
 void main() {
-    vClr = vec4(1.0, 0.0, 0.0, 1.0);
-
     // make vec4 with passed xyz and w = 1.0 for simplicity, w is used for like
     // projections and stuff? but its not needed rn
     vec4 pos = vec4(aPosition, 1.0);
@@ -19,11 +12,6 @@ void main() {
     // map tex coords (0, 0) to (1, 1)
     // to clip space (-1, -1) to (1, 1)
     pos.xy = pos.xy * 2. - 1.;
-
-    // make circle have padding
-    pos.xy *= 0.5;
-
-    pos.y += sin(millis/300. + pos.x * 20.)/20.;
 
     // NOTE: this setup currently stretches what is drawn
     // to fill the screen, to have the geometry use the 
