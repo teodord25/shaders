b11f6d43bdf90d6cfb9bf1ac1362718f8a255c32
tedord25 <djuric.teodor25@gmail.com>
2024-08-18 15:06:19 +0200

    repeating gradient

--- fragment.glsl
+++ fragment.glsl
@@ -8,13 +8,5 @@ out vec4 outClr;
 
 void main() {
 
-    vec4 tl = vec4(1.0, 0.0, 0.0, 1.0);
-    vec4 tr = vec4(0.0, 1.0, 0.0, 1.0);
-    vec4 bl = vec4(0.0, 0.0, 1.0, 1.0);
-    vec4 br = vec4(1.0, 1.0, 0.0, 1.0);
-
-    vec4 c1 = mix(tl, tr, vTexCoord.x);
-    vec4 c2 = mix(bl, br, vTexCoord.x);
-
-    outClr = mix(c2, c1, vTexCoord.y);
+    outClr = vec4(fract(vTexCoord * 10.0), 1.0, 1.0);
 }
