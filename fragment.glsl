d3a1f29872772636ccf3d6e57c75a6fcaa452ca5
tedord25 <djuric.teodor25@gmail.com>
2024-08-18 14:56:28 +0200

    custom 1d gradient

--- fragment.glsl
+++ fragment.glsl
@@ -8,5 +8,10 @@ out vec4 outClr;
 
 void main() {
 
-    outClr = vec4(vTexCoord, 1.0, 1.0);
+    vec4 c1 = vec4(1.0, 1.0, 0.0, 1.0);
+    vec4 c2 = vec4(0.0, 1.0, 1.0, 1.0);
+
+    // linearly interpolates between c1 and c2,
+    // vtexCoord.x is the index of where in the interpolation the output color should be
+    outClr = mix(c1, c2, vTexCoord.x);
 }
