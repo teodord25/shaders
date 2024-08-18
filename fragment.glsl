91fda19ec4dfe053d237852ef95532dd667f68bd
tedord25 <djuric.teodor25@gmail.com>
2024-08-18 18:48:35 +0200

    kinda colored

--- fragment.glsl
+++ fragment.glsl
@@ -11,14 +11,13 @@ in vec2 vTexCoord;
 out vec4 outClr;
 
 void main() {
-    float zoom = 3. * 1000. / millis;
+    float zoom = 2.;
     vec2 center = u_center;
-    center.x = center.x + 0.5 * log(millis / 1000.);
 
     vec2 c = (vTexCoord - 0.5) * zoom - center;
     vec2 z = vec2(0.0);
     int i;
-    for(i = 0; i < 1000; i++) {
+    for (i = 0; i < 1000; i++) {
         // z = z^2
         // (x + yi)^2 =
         // x^2 + 2xyi + y^2i^2 =
@@ -28,6 +27,13 @@ void main() {
         z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
         if (dot(z, z) > 4.0) break;
     }
-    float color = float(i) / 1000.0;
-    outClr = vec4(vec3(color), 1.0);
+    float t = float(i) / 1000.0;
+
+    vec3 color = vec3(
+            0.5 + 0.5 * cos(3.0 + t * 6.28318530718),
+            0.5 + 0.5 * cos(3.0 + t * 6.28318530718 + 2.09439510239),
+            0.5 + 0.5 * cos(3.0 + t * 6.28318530718 + 4.18879020479)
+        );
+
+    outClr = vec4(color, 1.0);
 }
