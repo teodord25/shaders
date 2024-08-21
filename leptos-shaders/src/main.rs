b4feb0c45ad37cae65f79a556d5bdba83bfbce66
tedord25 <djuric.teodor25@gmail.com>
2024-08-21 20:31:56 +0200

    basic leptos

--- leptos-shaders/src/main.rs
+++ leptos-shaders/src/main.rs
@@ -1,3 +1,58 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    let (toggled, set_toggled) = create_signal(false);
+
+    // share `set_toggled` with all children of this component
+    provide_context(set_toggled);
+
+    view! {
+        <p>"Toggled? " {toggled}</p>
+        <Layout/>
+    }
+}
+
+#[component]
+pub fn Layout() -> impl IntoView {
+    view! {
+        <header>
+            <h1>"My Page"</h1>
+        </header>
+        <main>
+            <Content/>
+        </main>
+    }
+}
+
+#[component]
+pub fn Content() -> impl IntoView {
+    view! {
+        <div class="content">
+            <ButtonD/>
+        </div>
+    }
+}
+
+#[component]
+pub fn ButtonD() -> impl IntoView {
+    // use_context searches up the context tree, hoping to
+    // find a `WriteSignal<bool>`
+    // in this case, I .expect() because I know I provided it
+    let setter = use_context::<WriteSignal<bool>>()
+        .expect("to have found the setter provided");
+
+    view! {
+        <button
+            on:click=move |_| setter.update(|value| *value = !*value)
+        >
+            "Toggle"
+        </button>
+    }
 }
