5d7a4a440f17c13b66006bd285ac440d7d48de0f
tedord25 <djuric.teodor25@gmail.com>
2024-08-23 17:11:29 +0200

    impl redrawing new pos every click

--- leptos-shaders/src/main.rs
+++ leptos-shaders/src/main.rs
@@ -4,24 +4,33 @@ mod webgl;
 fn main() {
     console_error_panic_hook::set_once();
 
-    mount_to_body(|| view! { <App/> })
+    mount_to_body(|| {
+        view! {
+            <canvas id="canvas" width="640" height="480"></canvas>
+            <App/>
+        }
+    })
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+    let (count, set_count) = create_signal(0.0);
+    let (vertices, set_vertices) = create_signal([-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0]);
 
-    let vertices: [f32; 9] = [1.0, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
-
-    create_effect(move |_| { 
+    create_effect(move |_| {
         let context = webgl::init_webgl().unwrap();
-        webgl::draw(&context, vertices).unwrap();
+
+        webgl::draw(&context, vertices.get()).unwrap();
     });
 
     view! {
         <button on:click={move |_| {
-            set_count(count.get() + 1);
-        }}></button>
-        <canvas id="canvas" width="640" height="480"></canvas>
+            set_count(count.get() + 0.1);
+            set_vertices([
+                -0.7 + count.get(), -0.7, 0.0,
+                0.7, -0.7, 0.0,
+                0.0, 0.7, 0.0
+            ]);
+        }}>{"Draw"}</button>
     }
 }
