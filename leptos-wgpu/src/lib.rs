22933cc8c36aa8777eac370f8ade2cd31381c56f
tedord25 <djuric.teodor25@gmail.com>
2024-08-26 17:03:48 +0200

    move wgpu stuff to lib.rs; update toml

---
+++ leptos-wgpu/src/lib.rs
@@ -0,0 +1,40 @@
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let _ = event_loop.run(move |event, control_flow| match event {
+        // pattern match window events, and check if the window ids match
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            // inner pattern match on specific window events
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.exit(),
+            _ => {
+                println!("{:?}", event);
+                // all window events that are not CloseRequested or the
+                // specific keyboard input event end up here
+            }
+        },
+        // all non window events end up here
+        _ => {}
+    });
+}
+
